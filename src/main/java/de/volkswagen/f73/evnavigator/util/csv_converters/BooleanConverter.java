4c93259709b03d66463cd83a00dcbc1b1413ad6d
Thies Bücker <thies.buecker@volkswagen.de>
2022-01-26 11:22:49 +0100

    stated boolean converter

---
+++ src/main/java/de/volkswagen/f73/evnavigator/util/csv_converters/BooleanConverter.java
@@ -0,0 +1,10 @@
+package de.volkswagen.f73.evnavigator.util.csv_converters;
+
+import com.opencsv.bean.AbstractBeanField;
+
+/**
+ * @author Justo, David (SE-A/34)
+ * @author Bücker, Thies (SE-A/34)
+ */
+public class BooleanConverter extends AbstractBeanField<> {
+}
