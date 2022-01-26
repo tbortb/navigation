f9b3f4c342a3f142bc06b39382cc0522d4484aa3
Justo, David (SE-A/34) <david.justo@volkswagen.de>
2022-01-26 09:46:01 +0100

    Initial Commit

---
+++ src/main/java/de/volkswagen/f73/evnavigator/EvnavigatorApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.evnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvnavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvnavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/volkswagen/f73/evnavigator/EvnavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.evnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvnavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
